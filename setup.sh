1718cfba5e5ba1f9e5b19c6a77ece6a4b95eac66
Xiu Hong Kooi <kooixh@aurumn.io>
2025-01-26 13:41:43 +0800

    Add additional commands

--- setup.sh
+++ setup.sh
@@ -10,6 +10,20 @@ fi
 
 echo "Running setup.sh at $SCRIPT_DIR"
 
+echo "Installing oh-my-zsh"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Intalling alacritty"
+
+if [ ! -d "~/Applications/Alacritty.app" ]; then
+    brew install --cask alacritty
+fi
+
+
+brew install font-hack-nerd-font
+
+
 echo "Creating alacritty config directory"
 mkdir -p ~/.config/alacritty
 echo "Creating oh-my-posh config directory"
@@ -19,6 +33,8 @@ mkdir -p ~/.config/oh-my-posh
 cp ./.config/alacritty/alacritty.toml ~/.config/alacritty
 cp ./.config/oh-my-posh/zen.toml ~/.config/oh-my-posh
 
+
+echo "Replacing .zshrc"
 
 cp ./.zshrc ~/.zshrc
 
