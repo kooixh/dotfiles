9caa4f2414c46e30a4e105269e4714c523555913
Xiu Hong Kooi <kooixh@aurumn.io>
2025-01-26 16:20:48 +0800

    Restructure directories

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -z "${SCRIPT_DIR}" ]; then
     exit 1
 fi 
-
 
 echo "Running setup.sh at $SCRIPT_DIR"
 
@@ -14,28 +12,24 @@ echo "Installing oh-my-zsh"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Intalling alacritty"
+echo "Intalling alacritty..."
 
 if [ ! -d "~/Applications/Alacritty.app" ]; then
     brew install --cask alacritty
 fi
 
-
 brew install font-hack-nerd-font
 
-
-echo "Creating alacritty config directory"
+echo "Creating alacritty config directory..."
 mkdir -p ~/.config/alacritty
-echo "Creating oh-my-posh config directory"
+echo "Creating oh-my-posh config directory..."
 mkdir -p ~/.config/oh-my-posh
 
+cp ./terminal/alacritty/alacritty.toml ~/.config/alacritty
+cp ./prompt/oh-my-posh/zen.toml ~/.config/oh-my-posh
 
-cp ./.config/alacritty/alacritty.toml ~/.config/alacritty
-cp ./.config/oh-my-posh/zen.toml ~/.config/oh-my-posh
-
+echo "Copying p10k.zsh..."
+cp ./prompt/p10k/p10k.zsh ~/
 
 echo "Replacing .zshrc"
-
-cp ./.zshrc ~/.zshrc
-
-
+cp ./shell/.zshrc ~/.zshrc
